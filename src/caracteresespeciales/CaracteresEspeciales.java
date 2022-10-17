666759ca5044a433e0f55b2df575fad4590a8799
lizethdd <keeylic@outlook.es>
2022-10-17 20:01:09 +0200

    Done

---
+++ src/caracteresespeciales/CaracteresEspeciales.java
@@ -0,0 +1,16 @@
+
+package caracteresespeciales;
+
+public class CaracteresEspeciales {
+
+
+    public static void main(String[] args) {
+       var centro="CEEP";
+        System.out.println("Salto de línea: \n"+centro);
+        System.out.println("Tabulador: \t"+centro);
+        System.out.println("Retroceso:  \b\b"+centro);
+        System.out.println("Comilla simple: \'"+centro+"'");
+        System.out.println("Comilla doble: \""+centro+"\"");
+    }
+    
+}
